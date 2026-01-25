c420dfd0a8b89c04556ddd17fb4a33cdd2aeace7
jarmo9977 <jarmo@meyer-zu-eissen.de>
2026-01-25 08:43:52 +0100

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -4,6 +4,9 @@ input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
 basic.showIcon(IconNames.Fabulous)
 basic.showString("Jarmo ")
 basic.showString("Papa ")
+for (let Index = 0; Index <= 11; Index++) {
+    basic.showNumber(Index)
+}
 basic.showLeds(`
     . . . . .
     . # . # .
