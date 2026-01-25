2ab41bc91706792c3e1a4fb5addcf35f2e569d8e
jarmo9977 <jarmo@meyer-zu-eissen.de>
2026-01-25 08:46:50 +0100

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,19 +1,21 @@
 input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
     basic.showNumber(input.temperature())
 })
+input.onGesture(Gesture.Shake, function () {
+    basic.showLeds(`
+        . . . . .
+        . # . # .
+        . . . . .
+        . # # # .
+        # . . . #
+        `)
+})
 basic.showIcon(IconNames.Fabulous)
 basic.showString("Jarmo ")
 basic.showString("Papa ")
 for (let Index = 0; Index <= 11; Index++) {
     basic.showNumber(Index)
 }
-basic.showLeds(`
-    . . . . .
-    . # . # .
-    . . . . .
-    . # # # .
-    # . . . #
-    `)
 basic.forever(function () {
 	
 })
