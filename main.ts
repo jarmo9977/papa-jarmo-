77f1ee8b79352325cd5241da72f6f3bac48f9659
jarmo9977 <jarmo@meyer-zu-eissen.de>
2026-01-25 08:51:18 +0100

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,9 @@
 input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
     basic.showNumber(input.temperature())
 })
+input.onButtonEvent(Button.B, input.buttonEventClick(), function () {
+    basic.showNumber(input.lightLevel())
+})
 input.onGesture(Gesture.Shake, function () {
     basic.showLeds(`
         . . . . .
@@ -17,5 +20,5 @@ for (let Index = 0; Index <= 11; Index++) {
     basic.showNumber(Index)
 }
 basic.forever(function () {
-	
+    basic.showIcon(IconNames.Happy)
 })
