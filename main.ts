c47dfa8d3d9cf09762c5092d7413f4a9298a4349
jarmo9977 <jarmo@meyer-zu-eissen.de>
2026-01-25 09:03:21 +0100

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,9 @@
 input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
     basic.showNumber(input.temperature())
 })
+input.onButtonEvent(Button.AB, input.buttonEventClick(), function () {
+    basic.showNumber(input.compassHeading())
+})
 input.onButtonEvent(Button.B, input.buttonEventClick(), function () {
     basic.showNumber(input.lightLevel())
 })
@@ -19,6 +22,7 @@ basic.showString("Papa ")
 for (let Index = 0; Index <= 11; Index++) {
     basic.showNumber(Index)
 }
+basic.showString("Ich  zelle  bis  3 . Dann machs BANG . KAPIRT !!!!! ")
 basic.forever(function () {
     basic.showIcon(IconNames.Happy)
 })
